a7369a23d98f7c76792dfd167ec88a9c18d65fb6
Peter Johnson <peter.johnson@lutheradvertising.co.uk>
2014-07-06 23:56:49 +0100

    fdg

---
+++ test/my_tests.adb
@@ -0,0 +1,16 @@
+-- my_tests.adb
+with Ahven;
+
+package body My_Tests is
+   procedure Initialize (T : in out Test) is
+   begin
+      Set_Name (T, "My tests");
+      Ahven.Framework.Add_Test_Routine
+        (T, Test_Addition'Access, "Addition");
+   end Initialize;
+
+   procedure Test_Addition is
+   begin
+      null;
+   end Test_Addition;
+end My_Tests;
